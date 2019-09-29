4a4addf55cf4610a182050bb5d0604434d8a030a
Alex Kovalyov <a4.kovalyov@itransition.com>
2019-09-29 23:17:46 +0300

    UseRouter hook was implemented.

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,29 @@
 import withLayout from '../src/components/Layout';
+import Link from 'next/link';
 
-const Page = () => <p>Hello Next.js</p>;
+interface IPost {
+  title?: any;
+}
 
-export default withLayout(Page);
+const PostLink: React.FC<IPost> = props => (
+  <li>
+    <Link href={`/post?title=${props.title}`}>
+      <a>{props.title}</a>
+    </Link>
+  </li>
+);
+
+const Blog = () => {
+  return (
+    <div>
+      <h1>My Blog</h1>
+      <ul>
+        <PostLink title="Hello Next.js" />
+        <PostLink title="Learn Next.js is awesome" />
+        <PostLink title="Deploy apps with Zeit" />
+      </ul>
+    </div>
+  );
+};
+
+export default withLayout(Blog)
